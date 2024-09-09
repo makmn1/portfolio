77e958fa24eff7ce886284c5aa6665ebc648abf2
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-09 17:52:38 -0400

    Setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {WorksComponent} from "./works/works.component";
+import {ResumeComponent} from "./resume/resume.component";
+import {ContactComponent} from "./contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "works", component: WorksComponent },
+  { path: "resume", component: ResumeComponent },
+  { path: "contact", component: ContactComponent },
+];
