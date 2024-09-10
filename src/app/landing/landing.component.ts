9f6f8bd2fe8968273d84f6b0708448e9cead1f0c
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 04:49:08 -0400

    Implement landing page mockup

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    NgOptimizedImage
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
