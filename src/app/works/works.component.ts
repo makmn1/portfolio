5f555ad6f0d4d36887cc176c086935653c55cabe
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 18:52:05 -0400

    Implement works page

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {HomeCardComponent} from "../home/home-card/home-card.component";
+import {WorkCardComponent} from "./work-card/work-card.component";
 
 @Component({
   selector: 'app-works',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeCardComponent,
+    WorkCardComponent
+  ],
   templateUrl: './works.component.html',
   styleUrl: './works.component.css'
 })
