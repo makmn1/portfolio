9fe72a9e55a38bfc60f0bf9e3eb65eb663e1c7a4
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 08:56:48 -0400

    Update landing to route to home

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     MatButton,
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
