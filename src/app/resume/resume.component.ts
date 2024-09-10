6868cdcf767d3ca139263c82236ec5e1b1cf8acb
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 15:21:55 -0400

    Implement Resume viewer page

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as PDFObject from 'pdfobject';
 
 @Component({
   selector: 'app-resume',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.loadPdf();
+  }
+
+  loadPdf(): void {
+    PDFObject.embed(
+      "assets/1705339770_FastTrack%20Blank%20Blankski%20Sept%202023%20-%20Mohamed%20Mohamed.pdf",
+      "#resume-view-container"
+    );
+  }
 }
