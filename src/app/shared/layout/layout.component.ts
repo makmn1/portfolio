c92b17f68db527f7ff7101647ff7aeae13b672b6
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 11:07:10 -0400

    Add custom navigation rail

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {NavigationComponent} from "../navigation/navigation.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavigationComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
+import {NavigationItemComponent} from "./navigation-item/navigation-item.component";
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatIcon,
+    NavigationItemComponent
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
