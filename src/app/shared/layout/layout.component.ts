50a127409829a09be81e2a2c45e7a1520aff8d9c
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 12:12:55 -0400

    Implement shared layout

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {NavigationComponent} from "../navigation/navigation.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
