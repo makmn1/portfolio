650b44a70e103971d6068b079f9fc67ffabe7437
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 12:12:47 -0400

    Implement footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
