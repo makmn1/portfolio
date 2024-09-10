539f3c10ce43411ba9fe4ecca2235425db7548fc
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-10 08:56:37 -0400

    Update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,20 @@ import {AboutComponent} from "./about/about.component";
 import {WorksComponent} from "./works/works.component";
 import {ResumeComponent} from "./resume/resume.component";
 import {ContactComponent} from "./contact/contact.component";
+import {LayoutComponent} from "./shared/layout/layout.component";
 
 export const routes: Routes = [
   { path: "", component: LandingComponent },
-  { path: "home", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "works", component: WorksComponent },
-  { path: "resume", component: ResumeComponent },
-  { path: "contact", component: ContactComponent },
+  {
+    path: "app",
+    component: LayoutComponent,
+    children: [
+      { path: "home", component: HomeComponent },
+      { path: "about", component: AboutComponent },
+      { path: "works", component: WorksComponent },
+      { path: "resume", component: ResumeComponent },
+      { path: "contact", component: ContactComponent },
+      { path: "**", redirectTo: "home", pathMatch: "full" }
+    ]
+  }
 ];
