d588a7a8d2a3d241e40e758a14b8e76c29a6c88d
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-09-11 13:03:22 -0400

    Update contact link to use router link

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {NgOptimizedImage} from "@angular/common";
-import {APP_BASE_HREF} from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     MatIcon,
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
-  contactLink = `${APP_BASE_HREF}/app/contact`
-
-}
+export class FooterComponent {}
